ab2677c4f969aa40e143205dcca116bb710997ec
pjende <p.l.h.jende@utwente.nl>
2017-08-18 12:55:10 +0200

    update

--- main.cpp
+++ main.cpp
@@ -11,7 +11,55 @@
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    int h;
+    matrix obj_points; //Matrix containing the object points
+    std::cout << "Welcome to a our simple Photogrammetry backprojection program \n";
+    std::cout << "Do you want to run the program in demo mode [*.csv-files with object coordinates are loaded automatically] \n";
+    std::cout << "or would you like to enter object coordinates manually? Press [1] for demo mode and [2] for manual mode\n";
+    std::cin >> h;
+
+    switch (h) {
+        case 1: std::cout << "You selected demo mode \n";
+            break;
+        case 2: std::cout << "You selected manual mode \n";
+
+            int numpoints;
+
+            std::cout << "How many points would you like to enter? \n"; std::cin >> numpoints;
+            //std::cout << "Please enter your points in the following format [X, Y, Z] \n";
+
+
+            for (int i = 0; i < numpoints; ++i) {
+
+                std::vector<double> row;
+
+                std::cout << "Enter an X coordinate\n";
+                double tempX;
+                std::cin >> tempX;
+                row.push_back(tempX);
+
+                std::cout << "Enter a Y coordinate\n";
+                double tempY;
+                std::cin >> tempY;
+                row.push_back(tempY);
+
+                std::cout << "Enter a Z coordinate\n";
+                double tempZ;
+                std::cin >> tempZ;
+                row.push_back(tempZ);
+
+
+                obj_points.push_back(row);
+            }
+
+            break;
+
+
+        default: std::cout << "Please enter either [1] or [2]. This program cannot make a decision for you. It is just as smart enough to multiply some matrices.  \n";
+            break;
+
+    }
+
 
 
     //Note: below is some example code, we should replace this by the menu structure that allows user input
@@ -42,7 +90,7 @@ int main()
 
     multiply(a,b,c);
 
-    printMatrix(c);
+    printMatrix(obj_points);
 
     return 0;
 }
