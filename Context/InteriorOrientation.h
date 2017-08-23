4c08be237f296caa68062f6a77bb4feb5cde53b7
Joep Lijnen <joeppie@gmail.com>
2017-08-23 23:05:05 +0200

    Fixed two missing comments on two classes.

--- Context/InteriorOrientation.h
+++ Context/InteriorOrientation.h
@@ -4,7 +4,7 @@
 
 #include "../util.h"
 
-/*!
+/**
  * Represents the Interior Orientation/Internal parameters corresponding to an image; camera/sensor information
  */
 class InteriorOrientation
--- UnitTests/RunTestCases.h
+++ UnitTests/RunTestCases.h
@@ -5,7 +5,9 @@
 #ifndef BACKPROJECT_RUNTESTCASES_H
 #define BACKPROJECT_RUNTESTCASES_H
 
-
+/**
+ * Main class for containing the test cases when running googletest unittests.
+ */
 class RunTestCases {
 
 };
